46ba5b0aa059e73f01d50725274ac2706087dcb1
olandan <landan.olivier@gmail.com>
2021-12-01 23:48:01 +0100

    Update tst.py

--- tst.py
+++ tst.py
@@ -3,6 +3,9 @@ import streamlit as st
 def f(x):
     return(2*x+3)
 
+def rev_f(y):
+    return((y-3)/2)
+
 st.write("""
 # Calculer une image
 Déterminer les images de nombres par la fonction définie par f(x)=2x+3
@@ -11,3 +14,12 @@ Déterminer les images de nombres par la fonction définie par f(x)=2x+3
 x = st.slider('Choisir un nombre entier entre -10 et 10', -10, 1, 10)
 
 st.write("L'image de ",x," par f(x)=2x+3 est f(",x,")=",str(f(x)))
+
+st.write("""
+# Calculer un antécédent
+Déterminer l'antécédent de nombres pour la fonction définie par f(x)=2x+3
+""")
+
+y = st.slider('Choisir un nombre entier entre -10 et 10', -10, 1, 10)
+
+st.write("L'antécédent de ",y," pour f(x)=2x+3 est f(",str(rev_f(y)),")=",y)
