f72a69e6d30ac7a9e30cfd137ccdf106ef526c2b
olandan <landan.olivier@gmail.com>
2022-04-27 22:40:25 +0200

    Update index.py

--- index.py
+++ index.py
@@ -4,6 +4,7 @@ from themes.algebre.algebre_2nde import *
 
 # Import des bibliothèques Python
 from sympy import *
+import streamlit as st
 
 # Zone de test
 # ============
@@ -14,11 +15,4 @@ L = liste_nombres(6,["Q" for i in range(6)])
 
 # Test de la classe créant une question d'algèbre
 # -----------------------------------------------
-for i in range(2):
-    for j in range(4):
-        for k in range(4):
-            q1 = quest_alg((i+1)*100+(j+1)*10+(k+1),L.nbs)
-            print(q1.quest)
-
-r=quest_alg(233,[3,5,7,9,11,13])
-print(r.quest)
+q1 = quest_alg((i+1)*100+(j+1)*10+(k+1),L.nbs)
